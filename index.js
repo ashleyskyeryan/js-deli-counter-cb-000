bdcaa4ac38da9d43439c3814d5a1f32de1e74e7a
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 22:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function currentLine(line) {
 if (line.length === 0) {
 return "The line is currently empty.";
 }
-else
+else {
 return "The line is currently: ${katzDeliLine.length + 1}, ${katzDeliLine.slice(1)}";
+}
 }
