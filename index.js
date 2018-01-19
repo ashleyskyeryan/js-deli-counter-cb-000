f393a56718822318159444d0892c0b5bf7c4fb06
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 22:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine > 0) {
+  return katzDeliLine[0]
+  katzDeliLine.shift
+}
+else
+return "There is nobody waiting to be served";
+}
