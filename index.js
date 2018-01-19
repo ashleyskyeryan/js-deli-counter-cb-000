b357c21928b39d07530dc0f8ba9dfa881d4fcd78
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 22:09:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
-   > 0) {
-  return katzDeliLine[0]
+  else  {
+  return "Currently serving ${katzDeliLine[0]}";
   katzDeliLine.shift
-}
-else {
-
 }
 }
