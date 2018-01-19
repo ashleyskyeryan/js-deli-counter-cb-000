14b260872cf3b8ccb1a9a63beaa4e9ba617b762a
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 21:57:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  var katzDeliLine = 0;
-  for (katzDeliLine = 0)
-  katzDeli.push("Welcome, ${name}. You are number ${number} in line")
-
+  katzDeliLine.push(name);
+  while(katzDeliLine < 0) {
+  katzDeliLine.push("Welcome, ${name}. You are number ${number} in line")
+}
 }
